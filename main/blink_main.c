6e532c4e67d89ea72b3ea8b4214d76a1ca719691
ewmeneses <will_menes@hotmail.com>
2022-10-06 17:23:02 -0500

    blink and button counter mas estético

--- main/blink_main.c
+++ main/blink_main.c
@@ -8,6 +8,7 @@
 
 #define BLINK_GPIO 26
 #define Button_GPIO 25
+int contador_pulsos=0;
 
 const char *TAG = "MAIN";
 TaskHandle_t controlador_t1=NULL;
@@ -20,20 +21,20 @@ void vTaskBlink(void *pvParameters)
 	/* Set the GPIO as a push/pull output */
 	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     while(1){
-    	ESP_LOGI(TAG,"encendiendo el led");
     	gpio_set_level(BLINK_GPIO, 1);
+    	ESP_LOGI(TAG,"LED:	1");
+    	//ESP_LOGI(TAG,"Estado del led: %d", gpio_get_level(BLINK_GPIO));
     	vTaskDelay(2000 / portTICK_PERIOD_MS);
 
-    	ESP_LOGW(TAG,"apagando el led");
     	gpio_set_level(BLINK_GPIO, 0);
+    	ESP_LOGI(TAG,"LED:	0");
+    	//ESP_LOGI(TAG,"Estado del led: %d", gpio_get_level(BLINK_GPIO));
     	vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
 
 void vTaskInput(void *pvParameters)
 {
-    int contador_pulsos=0;
-
 	//COnfiguración del pin
 	gpio_reset_pin(Button_GPIO);
 	gpio_set_direction(Button_GPIO, GPIO_MODE_INPUT);
@@ -41,14 +42,19 @@ void vTaskInput(void *pvParameters)
 
     while(1){
     	if (gpio_get_level(Button_GPIO)){
-    		ESP_LOGI(TAG,"Se ha presionado el boton");
     		contador_pulsos++;
-    		if (contador_pulsos>10){
-    			ESP_LOGE(TAG,"Se ha detenido la tarea button");
-    			vTaskDelete(controlador_t2);
-    		    contador_pulsos=0;
+    		if (contador_pulsos>2){
+    			contador_pulsos=0;
     		}
+    		ESP_LOGW(TAG,"Se ha presionado el boton. El contador de pulsos es %d",contador_pulsos);
     		while(gpio_get_level(Button_GPIO));
+    		if (contador_pulsos==0){
+    			vTaskResume(controlador_t1);
+    		    ESP_LOGW(TAG,"Todas las tareas estan activas");
+    		}else if (contador_pulsos==1){
+    		  	vTaskSuspend(controlador_t1);
+    		  	ESP_LOGW(TAG,"Se ha suspendido la tarea 1");
+    		}
     	}
     	vTaskDelay(50 / portTICK_PERIOD_MS);
     }
@@ -57,7 +63,7 @@ void vTaskInput(void *pvParameters)
 
 void app_main(void)
 {
-    //xTaskCreatePinnedToCore(vTaskBlink, "blink",2024,NULL,1,&controlador_t1,1);	//crearmos la tarea blink en core 1
+    xTaskCreatePinnedToCore(vTaskBlink, "blink",2024,NULL,1,&controlador_t1,1);	//crearmos la tarea blink en core 1
     xTaskCreatePinnedToCore(vTaskInput, "button",5024,NULL,1,&controlador_t2,1);
     //vTaskDelay(10000 / portTICK_PERIOD_MS);
     //vTaskDelete(controlador_t1);		//borramos la tarea blink
