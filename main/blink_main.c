ca1e8c159ba375313e4856eebf960b08d0f2d720
ewmeneses <will_menes@hotmail.com>
2022-10-07 09:29:25 -0500

    ejercicio finiquitado

--- main/blink_main.c
+++ main/blink_main.c
@@ -22,20 +22,20 @@ TaskHandle_t controlador_t3=NULL;	//handle task2
 
 void vTaskBlink(void *pvParameters)
 {
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
+	//configuración del pin de salida para el led
 	gpio_reset_pin(BLINK_GPIO);
-	/* Set the GPIO as a push/pull output */
 	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+	// parapadeo del led
     while(1){
     	gpio_set_level(BLINK_GPIO, 1);
     	ESP_LOGI(TAG,"LED:	1");
     	//ESP_LOGI(TAG,"Estado del led: %d", gpio_get_level(BLINK_GPIO));
-    	vTaskDelay(2000 / portTICK_PERIOD_MS);
+    	vTaskDelay(1000 / portTICK_PERIOD_MS);
 
     	gpio_set_level(BLINK_GPIO, 0);
     	ESP_LOGI(TAG,"LED:	0");
     	//ESP_LOGI(TAG,"Estado del led: %d", gpio_get_level(BLINK_GPIO));
-    	vTaskDelay(2000 / portTICK_PERIOD_MS);
+    	vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 
@@ -53,13 +53,23 @@ void vTaskInput(void *pvParameters)
     			contador_pulsos=0;
     		}
     		ESP_LOGW(TAG,"Se ha presionado el boton. El contador de pulsos es %d",contador_pulsos);
+
+    		//mientras se mantenga presionado el boton se queda en bucle infinito hasta soltarlo
     		while(gpio_get_level(Button_GPIO));
+
+    		//verificamos el contador para pausar o activar cada tarea
     		if (contador_pulsos==0){
     			vTaskResume(controlador_t1);
+    			vTaskResume(controlador_t2);
     		    ESP_LOGW(TAG,"Todas las tareas estan activas");
     		}else if (contador_pulsos==1){
     		  	vTaskSuspend(controlador_t1);
+    		  	vTaskResume(controlador_t2);
     		  	ESP_LOGW(TAG,"Se ha suspendido la tarea 1");
+    		}else if (contador_pulsos==2){
+    		  	vTaskSuspend(controlador_t2);
+    		  	vTaskResume(controlador_t1);
+    		  	ESP_LOGW(TAG,"Se ha suspendido la tarea 2");
     		}
     	}
     	vTaskDelay(50 / portTICK_PERIOD_MS);
@@ -69,24 +79,24 @@ void vTaskInput(void *pvParameters)
 
 void vTaskADC(void *vparameter){
 	//configuración del adc canal y resolución etc
-	adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
-	adc1_config_width(ADC_WIDTH_BIT_12);
+	adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11); 		//canal 4 correspondiente al pin 32, nivel de atenuación es el valor del voltaje de ref maxímo en este caso el nivel 11 corresponde a un max de 3500mV
+	adc1_config_width(ADC_WIDTH_BIT_12);								//es al resulción del adc, en este caso de 12 bits
 
-	//ahora si leemos el valor anlógico
-	int lectura_dig=0;
+	int lectura_dig=0;													//variable para guardar el valor del adc digital 0-4095
 	while(1){
 		lectura_dig=adc1_get_raw(ADC1_CHANNEL_4);
 		ESP_LOGI(TAG,"Lectura digital del adc: %i",lectura_dig);
-		vTaskDelay(500 / portTICK_PERIOD_MS);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
 
 }
 
 void app_main(void)
 {
-    //xTaskCreatePinnedToCore(vTaskBlink, "blink",2024,NULL,1,&controlador_t1,1);	//crearmos la tarea blink en core 1
-    //xTaskCreatePinnedToCore(vTaskInput, "button",5024,NULL,1,&controlador_t2,1);
-    xTaskCreatePinnedToCore(vTaskADC, "adc",5024,NULL,1,&controlador_t3,1);
+    xTaskCreatePinnedToCore(vTaskBlink, "blink",2024,NULL,1,&controlador_t1,1);	//crearmos la tarea blink en core 1
+    xTaskCreatePinnedToCore(vTaskADC, "adc",5024,NULL,1,&controlador_t2,1);
+    xTaskCreatePinnedToCore(vTaskInput, "button",5024,NULL,1,&controlador_t3,1);
+
     //vTaskDelay(10000 / portTICK_PERIOD_MS);
     //vTaskDelete(controlador_t1);		//borramos la tarea blink
     //vTaskSuspend(controlador_t1);		//pausamos la ejecución de la tarea blink
